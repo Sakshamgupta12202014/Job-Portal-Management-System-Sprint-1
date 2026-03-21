46ac1b5972c733dcc4fffd17b2958bcfed60b640
Sakshamgupta12202014 <gupta.saksham0401@gmail.com>
2026-03-22 01:32:35 +0530

    feat: fixed pom dependencies

--- JPMS-JobService/src/main/java/com/jobportal/JpmsJobServiceApplication.java
+++ JPMS-JobService/src/main/java/com/jobportal/JpmsJobServiceApplication.java
@@ -8,6 +8,7 @@ public class JpmsJobServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpmsJobServiceApplication.class, args);
+		System.out.println("Job service started ");
 	}
 
 }
